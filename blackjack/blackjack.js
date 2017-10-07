858d37aaf8ea084b0f3445a5b4db9dee87c1a3f1
R-Brewster <rbrewster@ucmerced.edu>
2017-10-06 22:13:53 -0700

    Started function planning

---
+++ blackjack/blackjack.js
@@ -0,0 +1,71 @@
+//****************************************Model****************************************//
+
+
+/***************************************************************************************************
+ * Deck - makes a deck of cards in random order
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+/***************************************************************************************************
+ * CardCompare - Compares the cards dealt to see if the player wins, loses, or gets blackjack
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+//**************************************Controller************************************//
+
+
+/***************************************************************************************************
+ * whatDealt - Tells the view what cards have been dealt
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+/***************************************************************************************************
+ * dealCards - Tells the model to deal cards
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+/***************************************************************************************************
+ * reset - Tells the model when to make a new deck of cards when the game is reset
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+//*****************************************View**************************************//
+
+
+/***************************************************************************************************
+ * showCards - Show the cards made
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+/***************************************************************************************************
+ * button - Button to reset game
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+
+/***************************************************************************************************
+ * dealCards - Tells model to deal cards
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
